a3999b443b53b0e629a67fe09ebb79a18209d66b
JayHu <2949763264@qq.com>
2026-09-10 19:13:14 +0800

    feat: JaySite 站点管理员过滤，新增 Blog 入口
    - configs/home.json: sites 新增博客条目，requiresAdmin: true
    - JaySite.vue: visibleSites computed 按 auth.isAdmin() 过滤 requiresAdmin 站点

--- src/components/home/JaySite.vue
+++ src/components/home/JaySite.vue
@@ -6,19 +6,26 @@
  */
 import * as icons from '@vicons/material'
 import type { Component } from 'vue'
-import { useGeneralStore } from '../../stores'
+import { computed } from 'vue'
+import { useGeneralStore, useAuthStore } from '../../stores'
 import homeConfig from '../../configs/home.json'
 
 const general = useGeneralStore()
+const auth = useAuthStore()
 
 // 根据图标名字符串渲染对应图标组件，找不到时用 WarningRound 兜底
 const renderIcon = (icon: string): Component => {
   return icons[icon as keyof typeof icons] || icons['WarningRound']
 }
+
+// 过滤站点：requiresAdmin 的站点仅管理员可见
+const visibleSites = computed(() =>
+  homeConfig.sites.filter((s) => !s.requiresAdmin || auth.isAdmin())
+)
 </script>
 <template>
   <n-card v-if="!general.isMobile" size="medium" title="站点" content-class="w-full grid gap-2 grid-cols-[repeat(auto-fill,minmax(200px,1fr))]">
-    <template v-for="site in homeConfig.sites" :key="site.router">
+    <template v-for="site in visibleSites" :key="site.router">
       <router-link v-if="site.type === 'router' && site.router" :to="site.router">
         <n-card v-magnetic size="medium" content-class="flex justify-center items-center gap-2 flex-row">
           <n-icon :size="30"><component :is="renderIcon(site.icon)" /></n-icon>
@@ -26,7 +33,7 @@ const renderIcon = (icon: string): Component => {
         </n-card>
       </router-link>
     </template>
-    <template v-for="link in homeConfig.sites" :key="link.link">
+    <template v-for="link in visibleSites" :key="link.link">
       <a v-if="link.type === 'link' && link.link" :href="link.link" target="_blank">
         <n-card v-magnetic class="w-full h-full" size="medium" content-class="flex justify-center items-center gap-2 flex-row">
           <n-icon :size="30"><component :is="renderIcon(link.icon)" /></n-icon>
@@ -37,7 +44,7 @@ const renderIcon = (icon: string): Component => {
   </n-card>
   <n-card v-else size="small" title="站点">
     <n-carousel show-arrow :slides-per-view="3" :space-between="8" class="pb-12">
-      <template v-for="site in homeConfig.sites" :key="site.router">
+      <template v-for="site in visibleSites" :key="site.router">
         <router-link v-if="site.type === 'router' && site.router" :to="site.router">
           <n-card v-interaction size="small" content-class="flex justify-center items-center gap-2 flex-col">
             <n-icon :size="27"><component :is="renderIcon(site.icon)" /></n-icon>
@@ -45,7 +52,7 @@ const renderIcon = (icon: string): Component => {
           </n-card>
       </router-link>
     </template>
-    <template v-for="link in homeConfig.sites" :key="link.link">
+    <template v-for="link in visibleSites" :key="link.link">
       <a v-if="link.type === 'link' && link.link" :href="link.link" target="_blank">
           <n-card v-interaction class="w-full h-full" size="small" content-class="flex justify-center items-center gap-2 flex-col">
             <n-icon :size="27"><component :is="renderIcon(link.icon)" /></n-icon>
